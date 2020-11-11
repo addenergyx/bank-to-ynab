8c17cb7f98d506fde36c3350f475a6098a1a4873
David <david-adeniji@hotmail.co.uk>
2020-11-11 21:31:13 +0000

    Clean up

--- hotlist.py
+++ hotlist.py
@@ -10,17 +10,16 @@ Created on Tue Nov 10 20:56:03 2020
 import pandas as pd
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoSuchElementException
 from fake_useragent import UserAgent
 import time
 from datetime import date, datetime
 from webdriver_manager.chrome import ChromeDriverManager
 import re
-#import os.path
 
 ## Trading 212 hot list
 ## On 2nd November Trading 212 added a popularity tracker to their site
-## Can You Predict Stock Price Movements With Users’ Behavior?
+## Can You Predict Stock Price Movements With Users’ Behaviour data?
 ## T212 doesn't store historical data so will be scrapping site daily and storing the results in a csv
 ## Trading 212 uses javascript to load the data so have to use selenium instead of beautifulsoup
 
@@ -34,20 +33,7 @@ fallers = pd.read_csv('fallers.csv')
 
 columns = ['Stock', 'Position', 'Start', 'End', 'Date', 'User_change', 'Percentage_change', 'Last_updated']
 
-# if os.path.isfile(f'leaderboard_{timestamp}.csv'):
-#     leaderboard = pd.read_csv(f'leaderboard_{timestamp}.csv')
-# else:
-#     leaderboard = pd.DataFrame(columns=['Stock', 'Position', 'User_count', 'Date', 'Last_updated'])
-
-# if os.path.isfile(f'leaderboard_{timestamp}.csv'):
-#     risers = pd.read_csv(f'risers_{timestamp}.csv')
-# else:
-#     risers = pd.DataFrame(columns=['Stock', 'Position', 'Start', 'End', 'Date', 'User_change', 'Percentage_change', 'Last_updated'])
-    
-# if os.path.isfile(f'fallers_{timestamp}.csv'):
-#     fallers = pd.read_csv(f'fallers_{timestamp}.csv')
-# else:
-#     fallers = pd.DataFrame(columns=['Stock', 'Position', 'Start', 'End', 'Date', 'User_change', 'Percentage_change', 'Last_updated'])
+## ------------------------- Selenium Setup ------------------------- ##
 
 def get_driver():
     options = Options()
@@ -64,13 +50,15 @@ def get_driver():
 
 driver = get_driver()
 
+## ------------------------- Leaderboard ------------------------- ##
+
 daily_hotlist = []
 
 driver.get(f'https://www.trading212.com/en/hotlist')
 
 time.sleep(5) # Pause for page to load
 
-a = driver.find_elements_by_class_name("pt-popularity-content-item")
+elements = driver.find_elements_by_class_name("pt-popularity-content-item")
 
 def get_last_update(update):
     match = re.search(r'\d{2}/\d{2}/\d{4}, \d{2}:\d{2}:\d{2}', update)
@@ -81,7 +69,7 @@ def get_last_update(update):
 update = driver.find_element_by_class_name("pt-footer-notice").text
 last_update = get_last_update(update)
 
-for stock in a:
+for stock in elements:
     daily_hotlist.append([stock.find_element_by_class_name('pt-name').text, 
             stock.find_element_by_class_name('pt-number').text, 
             stock.find_element_by_class_name('pt-holders-count').text, 
@@ -90,7 +78,6 @@ for stock in a:
 
 ## Direct correlation between position and user count so should remove position for model
 data = pd.DataFrame(daily_hotlist, columns=['Stock', 'Position', 'User_count', 'Date', 'Last_updated'])
-#data['Last_updated'] = pd.to_datetime(data['Last_updated'])
 data['User_count'] = data['User_count'].str.replace(',', '').astype(float)
 
 df = pd.concat([data, leaderboard], ignore_index=True)
@@ -107,12 +94,7 @@ complete_df = df.sort_values('User_count', ascending=False).drop_duplicates(['St
 complete_df['Position'] = list(complete_df.index+1)
 complete_df.to_csv('leaderboard.csv', index=False)
 
-## This script should only run once a day as a cron job but if ran more then once this will pervent duplicate entries
-# if not leaderboard[-100:].equals(data):
-#     leaderboard = pd.concat([leaderboard, data])
-#     leaderboard.to_csv('leaderboard.csv', index=False)
-
-## ------------------------- Risers/Fallers ------------------------- ##
+## ------------------------- Daily Risers/Fallers ------------------------- ##
 
 def user_data(xpath, file, historical_df):
     daily = []
@@ -124,9 +106,9 @@ def user_data(xpath, file, historical_df):
     update = driver.find_element_by_class_name("pt-footer-notice").text
     last_update = get_last_update(update)
     
-    a = driver.find_elements_by_class_name("pt-popularity-content-item")
+    elements = driver.find_elements_by_class_name("pt-popularity-content-item")
     
-    for stock in a[1:]:
+    for stock in elements[1:]:
         daily.append([stock.find_element_by_class_name('pt-name').text, 
                 stock.find_element_by_class_name('pt-number').text, 
                 stock.find_element_by_class_name('pt-change').text,
@@ -159,41 +141,7 @@ risers_df = user_data("/html/body/div[1]/section[2]/div/div/div[1]/div/div[2]", 
 
 fallers_df = user_data("/html/body/div[1]/section[2]/div/div/div[1]/div/div[3]", 'fallers.csv', fallers)
 
-## ------------------------- Fallers ------------------------- ##
-
-# daily_fallers = []
-
-# driver.find_element_by_xpath("/html/body/div[1]/section[2]/div/div/div[1]/div/div[3]").click()
-
-# time.sleep(5) # Pause for page to load
-
-# update = driver.find_element_by_class_name("pt-footer-notice").text
-# last_update = get_last_update(update)
-
-# a = driver.find_elements_by_class_name("pt-popularity-content-item")
-
-# for stock in a[1:]:
-#     daily_fallers.append([stock.find_element_by_class_name('pt-name').text, 
-#             stock.find_element_by_class_name('pt-number').text, 
-#             stock.find_element_by_class_name('pt-change').text,
-#             stock.find_element_by_class_name('pt-start').text,
-#             stock.find_element_by_class_name('pt-end').text,
-#             timestamp,
-#             last_update])
-
-# data = pd.DataFrame(daily_fallers)
-
-# data[['User_change','Percentage_change']] = data[2].str.split(' ', expand=True)
-# data = data.drop(2, 1) # inplace=True not working
-# data['Percentage_change'] = data['Percentage_change'].str.strip('()') # Remove brackets
-# data.columns = fallers.columns
-# data['Start'] = data['Start'].str.replace(',', '').astype(float)
-# data['End'] = data['End'].str.replace(',', '').astype(float)
-
-# if not fallers[-100:].equals(data):
-#     fallers = pd.concat([fallers, data])
-#     fallers.to_csv('fallers.csv', index=False)
-
+## ------------------------- Selenium Shutdown ------------------------- ##
 
 driver.close()
 driver.quit()
