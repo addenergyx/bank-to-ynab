8783e66c33df87292ec6bdbbcc26945efe6cb1ee
David <david-adeniji@hotmail.co.uk>
2021-01-19 21:47:20 +0000

    Improved yahoo finance ticker finder

--- live_portfolio.py
+++ live_portfolio.py
@@ -20,7 +20,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import time
 from sqlalchemy import create_engine
-import re 
+import re
+import difflib
+from helpers import get_market, get_yf_symbol
 
 load_dotenv(verbose=True, override=True)
 
@@ -66,6 +68,9 @@ columns = ['Ticker'] + [x.text for x in headers[0].find_elements_by_tag_name('th
 
 live_portfolio.columns = columns
 
+driver.close()
+driver.quit()
+
 # https://stackoverflow.com/questions/60030570/psycopg2-programmingerror-incomplete-placeholder-without
 # Fix potential SQL injection hole issue, doesn't like '%' in column name
 live_portfolio.rename(columns={'RESULT (%)':'RESULT_PCT'}, inplace=True)  
@@ -80,10 +85,56 @@ for col in cols:
     
 live_portfolio[cols] = live_portfolio[cols].apply(pd.to_numeric, errors='coerce')
 
-live_portfolio.to_sql('holdings', engine, if_exists='replace')
+live_portfolio['Sector'] = ''
+live_portfolio['Industry'] = ''
+live_portfolio['PREV_CLOSE'] = ''
+live_portfolio['YF_TICKER'] = ''
 
-driver.close()
-driver.quit()
+equities = pd.read_sql_table("equities", con=engine, index_col='index')
+companies = equities['COMPANY'].tolist()
+
+def get_sector(r):
+
+    # market = get_market(equities[equities['INSTRUMENT'] == r['Ticker']]['ISIN'].values[0], r['Ticker'])[1]
+    names = difflib.get_close_matches(r['INSTRUMENT'], companies, n=3, cutoff=0.3)
+    #names = difflib.get_close_matches('IAG SA', companies, n=3, cutoff=0.3)
+    #market = equities[equities['COMPANY'] == name and equities['INSTRUMENT'] == 'TSLA']  ['MARKET NAME'].values[0]
+    
+    print(r['INSTRUMENT']) 
+    
+    #ticker = 'NG'
+    try:
+        market = equities.query('COMPANY==@names and INSTRUMENT==@r.Ticker')['MARKET NAME'].values[0]
+    except IndexError:
+        market = equities.query('INSTRUMENT==@r.Ticker')['MARKET NAME'].values[0] # Assuming no duplicate tickers
+    
+    ticker = get_yf_symbol(market, r['Ticker'])
+    r['YF_TICKER'] = ticker
+    try:
+        
+        dic = yf.Ticker(ticker).info
+        
+        r.Sector = dic['sector']
+        r.Industry = dic['industry']
+        
+    except:
+        r.Sector = 'Uncategorised'
+        r.Industry = 'Uncategorised'
+        return r
+    return r
+
+live_portfolio = live_portfolio.apply(get_sector, axis=1)
+
+def prev_price(r):
+    try:
+        r['PREV_CLOSE'] = yf.download(tickers=r['YF_TICKER'], period='2d')['Adj Close'].values[0] #previous close
+    except IndexError:
+        pass
+    return r
+
+live_portfolio = live_portfolio.apply(prev_price, axis=1)
+
+live_portfolio.to_sql('holdings', engine, if_exists='replace')
 
 
 
